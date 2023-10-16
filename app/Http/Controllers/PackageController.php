f124c5884c5957ad051c4cc24ef75b2e22514791
Guilherme Andrade <42524262+guiandradedev@users.noreply.github.com>
2023-10-15 22:43:10 -0300

    feat: update image when store package

--- app/Http/Controllers/PackageController.php
+++ app/Http/Controllers/PackageController.php
@@ -7,51 +7,84 @@ use Illuminate\Http\Request;
 
 class PackageController extends Controller
 {
-    public function index(Package $package) {
+    public function index(Package $package)
+    {
         $packages = $package->all();
 
         return view('packages.index', compact('packages'));
     }
 
-    public function create() {
+    public function create()
+    {
         return view('packages.create');
     }
 
-    public function find(string|int $id, Package $pk) {
+    public function find(string|int $id, Package $pk)
+    {
         $package = $pk->find($id);
 
 
         return view('packages.show', compact('package'));
     }
 
-    public function store(Request $request, Package $package) {
-        $package->create($request->all());
+    public function store(Request $request, Package $package)
+    {
+
+        // $request->validate([
+        //     'images' => 'required',
+        //     'images.*' => 'required|image|mimes:png,jpg,jpeg|max:2048',
+        // ]);
+        // ^^ fazer acima com o StorePackageRequest $request
+
+
+        // $file = $request->file('images');
+        // dd($file);
+        // $extension = $file->getClientOriginalExtension();
+        // dd($file, $extension);
+        $images = [];
+        if ($request->has('images')) {
+            if (count($request->images) !== 3) {
+                return redirect()->route('packages.index');
+            }
+            $image_index = 1;
+            foreach ($request->images as $key => $image) {
+                $imageName = time() . rand(1, 99) . '.' . $image->extension();
+                $image->move(storage_path('images'), $imageName);
+
+                $img_db = "photo_" . $image_index;
+                $image_index++;
+                $request->merge([$img_db => $imageName]);
+            }
+        }
+        $data = $request->except('images');
+        $package->create($data);
 
         return redirect()->route('packages.index');
     }
-    public function delete(Request $request, Package $package){
+    public function delete(Request $request, Package $package)
+    {
         $package->delete($request->id);
 
         return redirect()->route('packages.index');
-        
     }
-    public function edit(Request $request, Package $pk) {
-        if(!$package = $pk->find($request->id)){
+    public function edit(Request $request, Package $pk)
+    {
+        if (!$package = $pk->find($request->id)) {
             return back();
         }
 
         return view('packages.update', compact('package'));
     }
 
-    public function update(Request $request, Package $pk){
-        if(!$package = $pk->find($request->id)){
+    public function update(Request $request, Package $pk)
+    {
+        if (!$package = $pk->find($request->id)) {
             return back();
         }
-        
+
         $package->update($request->only([
-            'name_package','food_description','beverages_description','photo_1','photo_2','photo_3','slug'
+            'name_package', 'food_description', 'beverages_description', 'photo_1', 'photo_2', 'photo_3', 'slug'
         ]));
         return redirect()->route('packages.index');
-
     }
 }
--- resources/views/packages/create.blade.php
+++ resources/views/packages/create.blade.php
@@ -1,4 +1,4 @@
-<form action="{{ route('packages.store') }}" method="POST">
+<form action="{{ route('packages.store') }}" method="POST" enctype="multipart/form-data">>
 
     @csrf
 
@@ -6,14 +6,14 @@
     <input type="text" name="slug" placeholder="slug">
     <textarea name="food_description" id="" cols="30" rows="10" placeholder="food_description"></textarea>
     <textarea name="beverages_description" id="" cols="30" rows="10" placeholder="beverages_description"></textarea>
-{{-- 
-    <input type="file" name="photo_1" id="">
-    <input type="file" name="photo_2" id="">
-    <input type="file" name="photo_3" id=""> --}}
 
-    <input type="text" name="photo_1">
+    <input type="file" name="images[]" id="">
+    <input type="file" name="images[]" id="">
+    <input type="file" name="images[]" id="">
+
+    {{-- <input type="text" name="photo_1">
     <input type="text" name="photo_2">
-    <input type="text" name="photo_3">
+    <input type="text" name="photo_3"> --}}
 
     <button type="submit">Cadastrar</button>
 
