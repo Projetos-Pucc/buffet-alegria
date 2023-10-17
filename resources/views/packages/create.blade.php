60c9524e9075e8bd3881a3da3ae4a48b9a424635
Ximenes <135142421+Ximenninho@users.noreply.github.com>
2023-10-16 23:37:14 -0300

    🐛 fix- formulario
    agora, no create, a validação não remove os campos preenchidos

--- resources/views/packages/create.blade.php
+++ resources/views/packages/create.blade.php
@@ -9,10 +9,10 @@
 
     @csrf
 
-    <input type="text" name="name_package" placeholder="name_package">
-    <input type="text" name="slug" placeholder="slug">
-    <textarea name="food_description" id="" cols="30" rows="10" placeholder="food_description"></textarea>
-    <textarea name="beverages_description" id="" cols="30" rows="10" placeholder="beverages_description"></textarea>
+    <input type="text" name="name_package" placeholder="name_package" value="{{old('name_package')}}">
+    <input type="text" name="slug" placeholder="slug" value="{{old('slug')}}">
+    <textarea name="food_description" id="" cols="30" rows="10" placeholder="food_description"></textarea>{{old('food_description')}}
+    <textarea name="beverages_description" id="" cols="30" rows="10" placeholder="beverages_description"></textarea>{{old('beverages_description')}}
 
     <input type="file" name="images[]" id="">
     <input type="file" name="images[]" id="">
