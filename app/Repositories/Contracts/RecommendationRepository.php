341ddb6fdc8e3b0aa3f98a74170411a6bd37ca1a
Ximenes <135142421+Ximenninho@users.noreply.github.com>
2023-11-01 08:22:07 -0300

    Atualizações do Repository

--- app/Repositories/Contracts/RecommendationRepository.php
+++ app/Repositories/Contracts/RecommendationRepository.php
@@ -2,14 +2,16 @@
 
 namespace App\Repositories\Contract;
 
+use App\DTO\Recommendation\CreateRecommendationDTO;
+use App\DTO\Recommendation\UpdateRecommendationDTO;
 use stdClass;
 
 interface RecommendationRepository {
     public function getaAll(string $filter = null): array;
     public function findOneById(string $id): stdClass|null;
     public function findOne(...$filters): stdClass|null;
-    public function create();
+    public function create(CreateRecommendationDTO $dto): stdClass;
+    public function update(UpdateRecommendationDTO $dto): bool|null;
     public function delete(string $id): void;
-    public function update();
 
 }
