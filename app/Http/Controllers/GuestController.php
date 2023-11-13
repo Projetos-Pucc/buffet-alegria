be68c5deac8b95feab89d8c75f82c5861b02dca8
Ximenes <135142421+Ximenninho@users.noreply.github.com>
2023-11-12 23:03:03 -0300

    feat- controller

--- app/Http/Controllers/GuestController.php
+++ app/Http/Controllers/GuestController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\DTO\Guests\CreateGuestDTO;
+use App\DTO\Guests\UpdateGuestDTO;
+use App\Http\Requests\Guests\GuestsUpdateRequest;
 use App\Services\GuestService;
 use Illuminate\Http\Request;
 
@@ -9,43 +12,65 @@ class GuestController extends Controller
 {
     public function __construct(
         protected GuestService $service
-
     )
     {}
 
     public function index()
     {
+        $guests = $this->service->getAll();
+
+        return view('guests.index',compact('guests'));
 
     }
 
     public function create()
     {
+        return view('guests.create');
 
     }
 
-    public function find()
+    public function find(string $id)
     {
+        if(!$guests = $this->service->find($id)){
+            return back();
+        }
+        return view('gusets.show',compact('guest'));
+    }
+
+    public function store(GuestsUpdateRequest $request)
+    {
+        $this->service->create(CreateGuestDTO::makeFromRequest($request));
+
+        return redirect()->route('guests.index');
+    }
+
+    public function delete(Request $request)
+    {
+        $this->service->delete($request->id);
+
+        return redirect()->route('gusets.index');
 
     }
 
-    public function store()
+    public function update(GuestsUpdateRequest $request)
     {
-
+        $guest = $this->service->update(
+            UpdateGuestDTO::makeFromRequest($request)
+        );
+        if(!$guest){
+            return back();
+        }
+        
+        return redirect()->route('guests.index');
     }
 
-    public function delete()
+    public function edit(Request $request)
     {
+        if(!$guest = $this->service->find($request->id)) {
+            return back();
+        }
 
-    }
-
-    public function update()
-    {
-
-    }
-    
-    public function edit()
-    {
-
+        return view('guests.update',compact('guest'));
     }
     
 }
